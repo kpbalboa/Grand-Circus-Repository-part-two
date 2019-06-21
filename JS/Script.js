7711f1d6d29971a8d13effc7d3fee389891e8d26
kpbalboa <kpbalboa@gmail.com>
2019-06-21 14:02:06 -0400

    Updated to use prompts and declare var resaults earlier

--- JS/Script.js
+++ JS/Script.js
@@ -1,30 +1,26 @@
-function getValue(){
-var eventType  = document.getElementById("eventType").value;
-var tempFahr = document.getElementById("tempFahr").value;
-//'var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, ";
 
-  if (eventType==="casual" && tempFahr<54) {
-   var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear something comfy and a coat.";
-    }else if (eventType==='casual' &&  tempFahr<=70 && tempFahr>=53 ){
-    var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear something comfy and a Jacket.";
-  }else if (eventType==="casual" && tempFahr>70) {
-   var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear something comfy and no Jacket.";
-    }else if (eventType==="semi-formal" && tempFahr>70) {
-   var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a polo and no Jacket.";
-   }else if (eventType==="semi-formal" && tempFahr<54) {
-   var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a polo and a coat.";
-    }else if (eventType==="formal" && tempFahr<54) {
-   var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a suit and a coat.";
-   }else if (eventType==='semi-formal' &&  tempFahr<=70 && tempFahr>=53 ){
-    var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a polo and a Jacket.";
-     }else if (eventType==='formal' &&  tempFahr<=70 && tempFahr>=53 ){
-    var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a suit and a Jacket.";
-    }else if (eventType==="formal" && tempFahr>70) {
-   var results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a suit and no Jacket.";
+var eventType = prompt("Please enter the event type you will be attending.  Use the terms casual, semi-formal, or formal");  //= document.getElementById("eventType").value;
+var tempFahr = prompt("Please enter the temperature in degrees fahrenheit"); //= document.getElementById("tempFahr").value;
+var results; 
+
+  if (eventType=="casual" && tempFahr<54) {
+    results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear something comfy and a coat.";
+    }else if (eventType=='casual' &&  tempFahr<=70 && tempFahr>=53 ){
+     results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear something comfy and a Jacket.";
+    }else if (eventType=="casual" && tempFahr>70) {
+    results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear something comfy and no Jacket.";
+    }else if ((eventType=="semi-formal" || eventType=='semi formal')  && tempFahr>70) {
+    results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a polo and no Jacket.";
+    }else if ((eventType=="semi-formal" || eventType=='semi formal') && tempFahr<54) {
+    results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a polo and a coat.";
+    }else if (eventType=="formal" && tempFahr<54) {
+    results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a suit and a coat.";
+    }else if ((eventType=="semi-formal" || eventType=='semi formal') &&  tempFahr<=70 && tempFahr>=53 ){
+    results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a polo and a Jacket.";
+    }else if (eventType=='formal' &&  tempFahr<=70 && tempFahr>=53 ){
+    results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a suit and a Jacket.";
+    }else if (eventType=="formal" && tempFahr>70) {
+    results = "Since it is "+ tempFahr +" degrees fahrenheit, and you are attending a "+eventType+" event, "+ "you should wear a suit and no Jacket.";
     }
+
  console.log(results);
- delete results;
-  }
-  
- 
-  
